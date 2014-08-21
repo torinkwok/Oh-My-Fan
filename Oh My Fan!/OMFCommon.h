d99c8407727200c9fae9433dd6a043965d79fdce
Tong Guo <Tong-G@outlook.com>
2014-08-21 13:46:44 +0800

    Initial Commit

---
+++ Oh My Fan!/OMFCommon.h
@@ -0,0 +1,75 @@
+///:
+/*****************************************************************************
+ **                                                                         **
+ **                               .======.                                  **
+ **                               | INRI |                                  **
+ **                               |      |                                  **
+ **                               |      |                                  **
+ **                      .========'      '========.                         **
+ **                      |   _      xxxx      _   |                         **
+ **                      |  /_;-.__ / _\  _.-;_\  |                         **
+ **                      |     `-._`'`_/'`.-'     |                         **
+ **                      '========.`\   /`========'                         **
+ **                               | |  / |                                  **
+ **                               |/-.(  |                                  **
+ **                               |\_._\ |                                  **
+ **                               | \ \`;|                                  **
+ **                               |  > |/|                                  **
+ **                               | / // |                                  **
+ **                               | |//  |                                  **
+ **                               | \(\  |                                  **
+ **                               |  ``  |                                  **
+ **                               |      |                                  **
+ **                               |      |                                  **
+ **                               |      |                                  **
+ **                               |      |                                  **
+ **                   \\    _  _\\| \//  |//_   _ \// _                     **
+ **                  ^ `^`^ ^`` `^ ^` ``^^`  `^^` `^ `^                     **
+ **                                                                         **
+ **                       Copyright (c) 2014 Tong G.                        **
+ **                          ALL RIGHTS RESERVED.                           **
+ **                                                                         **
+ ****************************************************************************/
+
+#define __THROW_EXCEPTION__WHEN_INVOKED_PURE_METHOD__           \
+    @throw [ NSException exceptionWithName: NSGenericException  \
+                         reason: [ NSString stringWithFormat: @"unimplemented pure virtual method `%@` in `%@` " \
+                                                               "from instance: %p" \
+                                                            , NSStringFromSelector( _cmd )          \
+                                                            , NSStringFromClass( [ self class ] )   \
+                                                            , self ]                                \
+                         userInfo: nil ]
+
+
+
+#if DEBUG
+#   define __CAVEMEN_DEBUGGING__PRINT_WHICH_METHOD_INVOKED__   \
+        NSLog( @"-[ %@ %@ ] be invoked"                        \
+            , NSStringFromClass( [ self class ] )              \
+            , NSStringFromSelector( _cmd )                     \
+            )
+#else
+#   define __CAVEMEN_DEBUGGING__PRINT_WHICH_METHOD_INVOKED__
+#endif
+
+#define IBACTION_BUT_NOT_FOR_IB IBAction
+
+#define USER_DEFAULTS  [ NSUserDefaults standardUserDefaults ]
+#define NOTIFICATION_CENTER [ NSNotificationCenter defaultCenter ]
+
+////////////////////////////////////////////////////////////////////////////
+
+/****************************************************************************
+ **                                                                        **
+ **      _________                                      _______            **
+ **     |___   ___|                                   / ______ \           **
+ **         | |     _______   _______   _______      | /      |_|          **
+ **         | |    ||     || ||     || ||     ||     | |    _ __           **
+ **         | |    ||     || ||     || ||     ||     | |   |__  \          **
+ **         | |    ||     || ||     || ||     ||     | \_ _ __| |  _       **
+ **         |_|    ||_____|| ||     || ||_____||      \________/  |_|      **
+ **                                           ||                           **
+ **                                    ||_____||                           **
+ **                                                                        **
+ ***************************************************************************/
+///:~
